85a8448776bcbe4ab134a793c755b3d5cd5b4f8d
MojitoBar <wnehdtjr5@naver.com>
2018-09-16 19:10:54 +0900

    Add logoScene

--- app/src/main/java/com/example/judongseok/itcontest/MainActivity.java
+++ app/src/main/java/com/example/judongseok/itcontest/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.judongseok.itcontest;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -21,11 +24,10 @@ import java.util.ArrayList;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-    private String htmlPageUrl;  //파싱할 홈페이지의 URL주소
-    private TextView textviewHtmlDocument;
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    private String htmlPageUrl = null;  //파싱할 홈페이지의 URL주소
     private String htmlContentInStringFormat="";
-    private String urlPath="";
+    private String[] urlPath;
 
     // 크롤링 카운터
     int cnt=0;
@@ -36,31 +38,41 @@ public class MainActivity extends AppCompatActivity {
     // 장학공지 카운터
     int JanghakcNoticeCount= 0;
 
+    int currentPage = 1;
+
     // 공지들이 리스트 뷰로 보여지게 하는 변수
     public ArrayList<String> items;
     public ArrayAdapter adapter;
+    public ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textviewHtmlDocument = (TextView)findViewById(R.id.textView);
-        textviewHtmlDocument.setMovementMethod(new ScrollingMovementMethod()); //스크롤 가능한 텍스트뷰로 만들기
 
+        Intent intent = getIntent();
+        htmlPageUrl = intent.getStringExtra("HtmlUrl");
+
+        urlPath = new String[30];
 
         items = new ArrayList<String>() ;
         // ArrayAdapter 생성. 아이템 View를 선택(single choice)가능하도록 만듦.
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, items) ;
-
-
+        adapter = new ArrayAdapter(this, R.layout.mytextview, items) ;
 
         // listview 생성 및 adapter 지정.
-        final ListView listview = (ListView) findViewById(R.id.listview1) ;
+        listview = (ListView) findViewById(R.id.listview1);
         listview.setAdapter(adapter);
 
+        listview.setOnItemClickListener(this);
+
+        // 공지 다음으로 넘어가는 버튼
+        Button nextButton = (Button)findViewById(R.id.button4);
+
+        // 공지 이전으로 넘어가는 버튼
+        Button preButton = (Button)findViewById(R.id.button5);
 
         // 행사 공지 크롤링 버튼 생성
-        Button htmlTitleButton = (Button)findViewById(R.id.button);
+        final Button htmlTitleButton = (Button)findViewById(R.id.button);
 
         // 학사 공지 크롤링 버튼 생성
         Button hacksaNoticeButton = (Button)findViewById(R.id.button2);
@@ -68,14 +80,59 @@ public class MainActivity extends AppCompatActivity {
         // 장학 공지 크롤링 버튼 생성
         Button janghackNoticeButton = (Button)findViewById(R.id.button3);
 
+
+        // 다음으로 넘어가는 버튼 리스너
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentPage < 30 && htmlPageUrl != null){
+                    currentPage++;
+                    int idx = htmlPageUrl.indexOf("=");
+                    int z = htmlPageUrl.charAt(idx+1);
+                    System.out.print(z);
+                    z++;
+                    StringBuilder temp = new StringBuilder(htmlPageUrl);
+                    temp.setCharAt(idx+1, (char)z);
+                    htmlPageUrl = temp.toString();
+
+                    items.clear();
+
+                    JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+                    jsoupAsyncTask.execute();
+                }
+            }
+        });
+
+        preButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentPage > 1 && htmlPageUrl != null){
+                    currentPage--;
+                    int idx = htmlPageUrl.indexOf("=");
+                    int z = htmlPageUrl.charAt(idx+1);
+                    System.out.print(z);
+                    z--;
+                    StringBuilder temp = new StringBuilder(htmlPageUrl);
+                    temp.setCharAt(idx+1, (char)z);
+                    htmlPageUrl = temp.toString();
+
+                    items.clear();
+
+                    JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+                    jsoupAsyncTask.execute();
+                }
+            }
+        });
+
         // 행사공지 크롤링 버튼 리스너
         htmlTitleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // 띄워져 있는 공지 clear & 공지 1페이지로 초기화
                 items.clear();
+                currentPage = 1;
                 // 행사공지 URL 연결
-                htmlPageUrl = "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10008";
-                clearTextView();
+                htmlPageUrl = "http://www.skhu.ac.kr/board/boardlist.aspx?curpage=1&bsid=10008";
                 // 행사공지 카운트 초기화
                 ContestNoticeCount = 0;
                 System.out.println( (cnt+1) +"번째 파싱");
@@ -89,10 +146,11 @@ public class MainActivity extends AppCompatActivity {
         hacksaNoticeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // 띄워져 있는 공지 clear & 공지 1페이지로 초기화
                 items.clear();
+                currentPage = 1;
                 // 학사공지 URL 연결
                 htmlPageUrl = "http://www.skhu.ac.kr/board/boardlist.aspx?curpage=1&bsid=10004&searchBun=51";
-                clearTextView();
                 // 학사공지 카운트 초기화
                 HacksaNoticeCount = 0;
                 System.out.println( (cnt+1) +"번째 파싱");
@@ -106,10 +164,11 @@ public class MainActivity extends AppCompatActivity {
         janghackNoticeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // 띄워져 있는 공지 clear & 공지 1페이지로 초기화
                 items.clear();
+                currentPage = 1;
                 // 장학공지 URL 연결
                 htmlPageUrl = "http://www.skhu.ac.kr/board/boardlist.aspx?curpage=1&bsid=10006&searchBun=75";
-                clearTextView();
                 // 장학공지 카운트 초기화
                 JanghakcNoticeCount = 0;
                 System.out.println( (cnt+1) +"번째 파싱");
@@ -118,11 +177,19 @@ public class MainActivity extends AppCompatActivity {
                 cnt++;
             }
         });
+
+
+
+        // 맨 처음 실행되는 크롤링
+        JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+        jsoupAsyncTask.execute();
     }
 
-    void clearTextView(){
-        textviewHtmlDocument.setText("");
-        htmlContentInStringFormat = "";
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String c_list = urlPath[position];
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(c_list));
+        startActivity(intent);
     }
 
     private class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
@@ -167,12 +234,13 @@ public class MainActivity extends AppCompatActivity {
                 //테스트2
                 titles= doc.select("td.left15 a");
 
+                int i = 0;
                 System.out.println("-------------------------------------------------------------");
                 for(Element e: titles){
                     String href = e.attr("abs:href");
-                    urlPath += href + "\n";
+                    urlPath[i] = href;
+                    i++;
                 }
-                System.out.println(urlPath);
 //
 //                //테스트3
 //                titles= doc.select("li.section02 div.con h2.news-tl");
---
+++ app/src/main/java/com/example/judongseok/itcontest/NoticeButtons.java
@@ -0,0 +1,57 @@
+package com.example.judongseok.itcontest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by judongseok on 2018-09-16.
+ */
+
+public class NoticeButtons extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notice_buttons);
+
+        Button btn_janghack = (Button)findViewById(R.id.janghack);
+        Button btn_hengsa= (Button)findViewById(R.id.hengsa);
+        Button btn_hacksa= (Button)findViewById(R.id.hacksa);
+        Button btn_suep= (Button)findViewById(R.id.suep);
+
+        btn_hacksa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NoticeButtons.this, MainActivity.class);
+                intent.putExtra("HtmlUrl", "http://www.skhu.ac.kr/board/boardlist.aspx?curpage=1&bsid=10004&searchBun=51");
+                startActivity(intent);
+            }
+        });
+        btn_hengsa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NoticeButtons.this, MainActivity.class);
+                intent.putExtra("HtmlUrl", "http://www.skhu.ac.kr/board/boardlist.aspx?curpage=1&bsid=10008");
+                startActivity(intent);
+            }
+        });
+        btn_janghack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NoticeButtons.this, MainActivity.class);
+                intent.putExtra("HtmlUrl", "http://www.skhu.ac.kr/board/boardlist.aspx?curpage=1&bsid=10006&searchBun=75");
+                startActivity(intent);
+            }
+        });
+        btn_suep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NoticeButtons.this, MainActivity.class);
+                intent.putExtra("HtmlUrl", "http://www.skhu.ac.kr/board/boardlist.aspx?curpage=1&bsid=10005&searchBun=53");
+                startActivity(intent);
+            }
+        });
+    }
+}
