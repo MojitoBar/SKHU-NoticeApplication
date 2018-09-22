de213a5cee47cdedbf777961f2f9165fb237996f
MojitoBar <wnehdtjr5@naver.com>
2018-09-22 23:40:53 +0900

    Add SharedPreferences

--- app/src/main/java/com/example/judongseok/itcontest/MyService.java
+++ app/src/main/java/com/example/judongseok/itcontest/MyService.java
@@ -1,5 +1,6 @@
 package com.example.judongseok.itcontest;
 
+import android.app.Activity;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -14,10 +15,67 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.widget.Toast;
 
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+
+
 public class MyService extends Service {
+    private String htmlPageUrl = "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10008"; //파싱할 홈페이지의 URL주소
+    private TextView textviewHtmlDocument;
+    private String htmlContentInStringFormat="";
+
+
     NotificationManager Notifi_M;
     ServiceThread thread;
     Notification Notifi ;
+    MainActivity mainActivity;
+
+    // 크롤링 카운터
+    int cnt = 0;
+
+    private class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+
+                Document doc = Jsoup.connect(htmlPageUrl).get();
+                //테스트1
+                Elements titles= doc.select("td.left15");
+
+                for(Element e: titles){
+                    htmlContentInStringFormat += e.text().trim() + "\n";
+                    cnt++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+        }
+    }
+
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -26,6 +84,8 @@ public class MyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
+        mainActivity = new MainActivity();
         Notifi_M = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         myServiceHandler handler = new myServiceHandler();
         thread = new ServiceThread(handler);
@@ -40,20 +100,33 @@ public class MyService extends Service {
         thread = null;//쓰레기 값을 만들어서 빠르게 회수하라고 null을 넣어줌.
     }
 
+    void crawlingCheck(){
+
+        JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+        jsoupAsyncTask.execute();
+            System.out.println(mainActivity.ContestNoticeCount);
+            System.out.println(cnt);
+
+        if(mainActivity.ContestNoticeCount != cnt){
+
+        }
+    }
+
     class myServiceHandler extends Handler {
         @Override
         public void handleMessage(android.os.Message msg) {
             Intent intent = new Intent(MyService.this, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(MyService.this, 0, intent,PendingIntent.FLAG_UPDATE_CURRENT);
 
+            crawlingCheck();
+
             Notifi = new Notification.Builder(getApplicationContext())
-                    .setContentTitle("Content Title")
-                    .setContentText("Content Text")
+                    .setContentTitle("행사 공지 알림!")
+                    .setContentText("행사 공지가 업데이트 됐습니다.")
                     .setSmallIcon(R.drawable.icon_new_gry)
                     .setTicker("알림!!!")
                     .setContentIntent(pendingIntent)
                     .build();
-
             //소리추가
             Notifi.defaults = Notification.DEFAULT_SOUND;
 
